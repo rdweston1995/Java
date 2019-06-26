157334e37bb9da556e0992071e416dbcec3d8e03
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-06-25 18:42:55 -0700

    Updating String token file

--- hackerRank/challenges/src/challenges/JavaStringToken.java
+++ hackerRank/challenges/src/challenges/JavaStringToken.java
@@ -9,41 +9,21 @@ public class JavaStringToken {
 		Scanner scan = new Scanner(System.in);
 		String s = scan.nextLine();
 		
-//		System.out.println(output(s));
-		output(s);
-		//output1(s);
+		if(s.length() == 0) {
+			System.out.println(s.length());
+		}else {
+			output(s);
+		}
 		scan.close();
 	}
 	
 	public static void output(String s) {
-		//Need to remove the starting spaces and start the string at the first letter
-//		String str = s;
-//		int index = 1;
-//		while(str.indexOf(" ") == 0) {
-//			str = str.substring(index, str.length());
-//			System.out.println(str);
-//			index++;
-//		}
-		if(s.length() == 0) {
-			System.out.println(s.length());
-		}else {
-			s = s.trim();
-			String delims = "[\\s .,?!'_@]+";
-			String[] tokens = s.split(delims);
-			System.out.println(tokens.length);
-			for(int i = 0; i < tokens.length; i++) {
-				System.out.println(tokens[i]);
-			}
-		}
-		
-	}
-	
-	public static void output1(String s) {
-		String newS = s.replaceAll("\\s","");
-		System.out.println(newS);
-		
-		String newnewS = newS.replaceAll("\\,","");
-		System.out.println(newnewS);
-		
+		s = s.trim();
+		String delims = "[\\s .,?!'_@]+";
+		String[] tokens = s.split(delims);
+		System.out.println(tokens.length);
+		for(int i = 0; i < tokens.length; i++) {
+			System.out.println(tokens[i]);
+		}	
 	}
 }
