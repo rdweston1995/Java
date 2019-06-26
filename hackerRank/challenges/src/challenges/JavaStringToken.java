29de36b5a0d2576e4b6b699978e7830c5cb2ab7b
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-06-25 18:55:28 -0700

    Finish string tokens and now all test cases run correctly and return the correct answers.

--- hackerRank/challenges/src/challenges/JavaStringToken.java
+++ hackerRank/challenges/src/challenges/JavaStringToken.java
@@ -9,21 +9,29 @@ public class JavaStringToken {
 		Scanner scan = new Scanner(System.in);
 		String s = scan.nextLine();
 		
-		if(s.length() == 0) {
-			System.out.println(s.length());
-		}else {
-			output(s);
-		}
+//		if(s.length() == 0) {
+//			System.out.println(s.length());
+//		}else {
+//			output(s);
+//		}
+		output(s);
 		scan.close();
 	}
 	
 	public static void output(String s) {
 		s = s.trim();
-		String delims = "[\\s .,?!'_@]+";
-		String[] tokens = s.split(delims);
-		System.out.println(tokens.length);
-		for(int i = 0; i < tokens.length; i++) {
-			System.out.println(tokens[i]);
-		}	
+		System.out.println("'" + s + "'");
+		System.out.println("'" + s.substring(0, s.length()) + "'");
+		if(s.isEmpty()) {
+			System.out.println(0);
+		}else {
+			String delims = "[\\s .,?!'_@]+";
+			String[] tokens = s.split(delims);
+			System.out.println(tokens.length);
+			for(int i = 0; i < tokens.length; i++) {
+				System.out.println(tokens[i]);
+			}
+		}
+			
 	}
 }
